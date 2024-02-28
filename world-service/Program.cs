4f65699457dfd7e9fdba80ff477069bf266c8064
Ferry To <ferrywlto@gmail.com>
2024-02-28 23:04:52 +0800

    feat: Add request IP info in response. (v0.0.1)

--- world-service/Program.cs
+++ world-service/Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var config = new ConfigurationBuilder().AddEnvironmentVariables().Build();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,9 +24,19 @@ var statuses = new[]
     "Freezing", "Melting", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/world", () => {
+app.MapGet("/world", (HttpContext ctx) =>
+{
     var rng = new Random();
-    return $"The world is {statuses[rng.Next(statuses.Length)]}";
+    var localAddress = ctx.Connection?.LocalIpAddress?.ToString();
+    var localPort = ctx.Connection?.LocalPort.ToString();
+    var remoteAddress = ctx.Connection?.RemoteIpAddress?.ToString();
+    var remotePort = ctx.Connection?.RemotePort.ToString();
+    var sb = new StringBuilder();
+    sb.AppendLine($"The world is {statuses[rng.Next(statuses.Length)]}.");
+    sb.AppendLine($"Request Info: Local: {localAddress}:{localPort}, Remote: {remoteAddress}:{remotePort}");
+    sb.AppendLine($"Config Info: foo:{config["foo"]}");
+    
+    return sb.ToString();
 })
 .WithName("World")
 .WithOpenApi();
