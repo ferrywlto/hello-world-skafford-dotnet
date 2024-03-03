3562d3b640687f3b055c6411552073b22c1afa7e
Ferry To <ferrywlto@gmail.com>
2024-03-03 23:04:26 +0800

    feat: Add OpenTelementry into hello-service

--- hello-service/Program.cs
+++ hello-service/Program.cs
@@ -1,11 +1,32 @@
+using System.Diagnostics.Metrics;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Meter myMeter = new("skaffold-dotnet-hello");
+Counter<int> requestCounter = myMeter.CreateCounter<int>("hello-request-counter");
+
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(builder => builder
+        .AddService(serviceName: "OpenTelementry Getting Started"))
+        .WithMetrics(builder => builder
+            .AddMeter("skaffold-dotnet-hello")
+            .AddPrometheusExporter()
+            .AddAspNetCoreInstrumentation()
+            .AddConsoleExporter((exporterOptions, metricReaderOptions) =>
+            {
+                metricReaderOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 1000;
+            })
+        );
 
 var app = builder.Build();
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,7 +42,11 @@ configurationBuilder.AddEnvironmentVariables();
 var config = configurationBuilder.Build();
 var worldServiceAddress = config["WORLD_SERVICE_ADDRESS"];
 
-app.MapGet("/hello/{name}", (string name) => $"Hello {name}!")
+app.MapGet("/hello/{name}", (string name) =>
+{
+    requestCounter.Add(1);
+    return $"Hello {name}!";
+})
 .WithName("Hello")
 .WithOpenApi();
 
